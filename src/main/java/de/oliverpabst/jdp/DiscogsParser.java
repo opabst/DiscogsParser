84cc56871eb8a70bb6529e81ccd34135af5f008f
Oliver Pabst <oliver.pabst@mailbox.org>
2019-03-13 15:19:44 +0100

    - added new commandline options to configure the database connection

--- src/main/java/de/oliverpabst/jdp/DiscogsParser.java
+++ src/main/java/de/oliverpabst/jdp/DiscogsParser.java
@@ -1,5 +1,8 @@
 package de.oliverpabst.jdp;
 
+import de.oliverpabst.jdp.database.ConnectionParameters;
+import de.oliverpabst.jdp.database.SchemaDoesNotExistException;
+import de.oliverpabst.jdp.database.postgresql.PostgreSQLConnector;
 import de.oliverpabst.jdp.parser.ArtistParser;
 import de.oliverpabst.jdp.parser.LabelParser;
 import de.oliverpabst.jdp.parser.MasterParser;
@@ -13,8 +16,18 @@ public class DiscogsParser {
         Options options = new Options();
 
         Option dumpDate = new Option("d", "date", true, "Date of the creation of the dump (part of the filename)");
+        Option hostName = new Option("h", "hostname", true, "Name of the databaseserver");
+        Option dbPort = new Option("p", "password", true, "Port of the running database instance");
+        Option dbName = new Option("db", "database", true, "Name of the database");
+        Option dbUser = new Option("u", "username", true, "Database username");
+        Option dbPassword = new Option("pw", "password", true, "Corresponding password for database username");
         dumpDate.setRequired(true);
         options.addOption(dumpDate);
+        options.addOption(hostName);
+        options.addOption(dbPort);
+        options.addOption(dbName);
+        options.addOption(dbUser);
+        options.addOption(dbPassword);
 
         CommandLineParser clParser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -34,9 +47,24 @@ public class DiscogsParser {
         File mastersFile = new File("data_" + date + "/discogs_" + date + "_masters.xml");
         File releasesFile = new File("data_" + date + "/discogs_" + date + "_releases.xml");
 
-        //ArtistParser ap = new ArtistParser(artistsFile);
-        //LabelParser lp = new LabelParser(labelsFile);
-        //MasterParser mp = new MasterParser(mastersFile);
+        String hostname = cmdLine.getOptionValue("h");
+        String port = cmdLine.getOptionValue("p");
+        String db = cmdLine.getOptionValue("db");
+        String username = cmdLine.getOptionValue("u");
+        String password = cmdLine.getOptionValue("pw");
+
+        ConnectionParameters cParams = new ConnectionParameters(hostname, port, db, username, password);
+
+        try {
+            PostgreSQLConnector.getInstance().connect(cParams);
+        } catch (SchemaDoesNotExistException e) {
+            System.err.println(e.getMessage());
+            System.exit(1); // Exit because db schema does not exist in the specified database
+        }
+
+        ArtistParser ap = new ArtistParser(artistsFile);
+        LabelParser lp = new LabelParser(labelsFile);
+        MasterParser mp = new MasterParser(mastersFile);
         ReleaseParser rp = new ReleaseParser(releasesFile);
     }
 }
