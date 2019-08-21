05ab60fa25179db62019c51d4578a5b644e88b7e
Oliver Pabst <pabst@dbs.uni-hannover.de>
2019-08-21 14:55:51 +0200

    renamed function to a more fitting name

---
+++ db/postgresql/functions/calculate_reduction.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE FUNCTION discogs.calculate_reduction() RETURNS VOID AS $$
+DECLARE
+    table_cursor CURSOR FOR
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'discogs'
+          AND table_name NOT LIKE '%import%'
+          AND table_name <> 'processing_results';
+
+    v_table_name TEXT;
+
+    v_stmt TEXT;
+
+    v_imported_tuple_cnt INTEGER;
+    v_transformed_tuple_cnt INTEGER;
+BEGIN
+    DROP TABLE IF EXISTS discogs.processing_results;
+    CREATE TABLE discogs.processing_results (
+        import_table TEXT,
+        import_tuple_count INTEGER,
+        transformed_table TEXT,
+        transformed_tuple_count INTEGER,
+        reduction_cnt INTEGER,
+        reduction_pct NUMERIC(5,2));
+
+    OPEN table_cursor;
+    LOOP
+        FETCH table_cursor INTO v_table_name;
+        EXIT WHEN NOT FOUND;
+
+        v_stmt := 'SELECT COUNT(*) FROM discogs.' || v_table_name || '_import';
+        EXECUTE v_stmt INTO v_imported_tuple_cnt;
+        v_stmt := 'SELECT COUNT(*) FROM discogs.' || v_table_name;
+        EXECUTE v_stmt INTO v_transformed_tuple_cnt;
+
+        INSERT INTO discogs.processing_results (import_table, import_tuple_count, transformed_table, transformed_tuple_count, reduction_cnt, reduction_pct)
+            VALUES (v_table_name || '_import', v_imported_tuple_cnt, v_table_name, v_transformed_tuple_cnt, v_imported_tuple_cnt - v_transformed_tuple_cnt, 100.00 -  TRUNC(cast(v_transformed_tuple_cnt AS DECIMAL) / (v_imported_tuple_cnt/100), 2));
+    END LOOP;
+    CLOSE table_cursor;
+END;
+$$language plpgsql;
