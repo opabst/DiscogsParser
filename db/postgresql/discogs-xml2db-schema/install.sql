0baed8c344a31aab7f533783a8d04fa25328de25
Oliver Pabst <opabst@icloud.com>
2019-10-22 07:42:12 +0200

    added formatting

--- db/postgresql/discogs-xml2db-schema/install.sql
+++ db/postgresql/discogs-xml2db-schema/install.sql
@@ -1,205 +1,181 @@
 CREATE SCHEMA discogs;
 
 CREATE TABLE discogs.artist (
-id integer NOT NULL,
-name text NOT NULL,
-realname text,
-urls text[],
-namevariations text[],
-aliases text[],
-releases integer[],
-profile text,
-members text[],
-groups text[],
-data_quality text,
-CONSTRAINT artist_pkey PRIMARY KEY (id));
+    id integer NOT NULL,
+    name text NOT NULL,
+    realname text,
+    urls text[],
+    namevariations text[],
+    aliases text[],
+    releases integer[],
+    profile text,
+    members text[],
+    groups text[],
+    data_quality text,
+    CONSTRAINT artist_pkey PRIMARY KEY (id));
 
 CREATE TABLE discogs.artists_image(
-
-artist_id integer,
-type text,
-height integer,
-width integer,
-image_uri text);
+    artist_id integer,
+    type text,
+    height integer,
+    width integer,
+    image_uri text);
 
 CREATE TABLE discogs.country (
-
-name text);
+    name text);
 
 CREATE TABLE discogs.format(
-
-name text NOT NULL,
-CONSTRAINT format_pkey PRIMARY KEY (name));
+    name text NOT NULL,
+    CONSTRAINT format_pkey PRIMARY KEY (name));
 
 CREATE TABLE discogs.genre (
-
-id integer NOT NULL,
-name text COLLATE,
-parent_genre integer,
-sub_genre integer,
-CONSTRAINT genre_pkey PRIMARY KEY (id));
+    id integer NOT NULL,
+    name text,
+    parent_genre integer,
+    sub_genre integer,
+    CONSTRAINT genre_pkey PRIMARY KEY (id));
 
 CREATE TABLE discogs.label (
-
-id integer NOT NULL,
-name text NOT NULL,
-contactinfo text,
-profile text,
-parent_label text,
-sublabels text[],
-urls text[],
-data_quality text,
-CONSTRAINT label_pkey PRIMARY KEY (id));
+    id integer NOT NULL,
+    name text NOT NULL,
+    contactinfo text,
+    profile text,
+    parent_label text,
+    sublabels text[],
+    urls text[],
+    data_quality text,
+    CONSTRAINT label_pkey PRIMARY KEY (id));
 
 CREATE TABLE discogs.labels_images (
-
-label_id integer,
-type text,
-height integer,
-width integer,
-image_uri text);
+    label_id integer,
+    type text,
+    height integer,
+    width integer,
+    image_uri text);
 
 CREAE TABLE discogs.master (
-
-id integer NOT NULL,
-title text,
-main_release integer NOT NULL,
-year integer,
-notes text,
-genres text,
-styles text,
-role text,
-data_quality text,
-CONSTRAINT master_pkey PRIMARY KEY (id))
+    id integer NOT NULL,
+    title text,
+    main_release integer NOT NULL,
+    year integer,
+    notes text,
+    genres text,
+    styles text,
+    role text,
+    data_quality text,
+    CONSTRAINT master_pkey PRIMARY KEY (id))
 
 CREATE TABLE discogs.master_artists (
-
-artist_name text,
-master_id integer);
+    artist_name text,
+    master_id integer);
 
 CREATE TABLE discogs.masters_artists_joins (
-
-artist1 text,
-artist2 text,
-join_relation text,
-master_id integer);
-
+    artist1 text,
+    artist2 text,
+    join_relation text,
+    master_id integer);
 
 CREATE TABLE discogs.masters_extraartists (
-
-master_id integer,
-artist_name text,
-roles text[]);
+    master_id integer,
+    artist_name text,
+    roles text[]);
 
 CREATE TABLE discogs.masters_formats (
-
-master_id integer,
-format_name text,
-qty integer,
-descriptions text[]);
+    master_id integer,
+    format_name text,
+    qty integer,
+    descriptions text[]);
 
 CREATE TABLE discogs.masters_images (
-
-master_id integer NOT NULL,
-type text COLLATE,
-height integer,
-width integer,
-image_uri text,
-CONSTRAINT masters_images_pkey PRIMARY KEY (master_id, type));
+    master_id integer NOT NULL,
+    type text COLLATE,
+    height integer,
+    width integer,
+    image_uri text,
+    CONSTRAINT masters_images_pkey PRIMARY KEY (master_id, type));
 
 CREATE TABLE discogs.release (
-
-id integer NOT NULL,
-status text,
-title text,
-country text,
-released text,
-barcode text,
-notes text,
-genres text,
-styles text,
-master_id integer,
-data_quality text,
-CONSTRAINT release_pkey PRIMARY KEY (id));
+    id integer NOT NULL,
+    status text,
+    title text,
+    country text,
+    released text,
+    barcode text,
+    notes text,
+    genres text,
+    styles text,
+    master_id integer,
+    data_quality text,
+    CONSTRAINT release_pkey PRIMARY KEY (id));
 
 CREATE TABLE discogs.releases_artists (
-
-release_id integer NOT NULL,
-"position" integer NOT NULL,
-artist_id integer,
-artist_name text,
-anv text,
-join_relation text,
-    CONSTRAINT releases_artists_pkey PRIMARY KEY (release_id, "position"));
+    release_id integer NOT NULL,
+    "position" integer NOT NULL,
+    artist_id integer,
+    artist_name text,
+    anv text,
+    join_relation text,
+        CONSTRAINT releases_artists_pkey PRIMARY KEY (release_id, "position"));
 
 CREATE TABLE discogs.releases_extraartists (
-
-release_id integer,
-artist_id integer,
-artist_name text,
-anv text,
-role text);
+    release_id integer,
+    artist_id integer,
+    artist_name text,
+    anv text,
+    role text);
 
 CREATE TABLE discogs.releases_formats (
-
-release_id integer NOT NULL,
-"position" integer NOT NULL,
-format_name text,
-qty numeric(100,0),
-descriptions text[],
-CONSTRAINT releases_formats_pkey PRIMARY KEY (release_id, "position"),
-CONSTRAINT releases_formats_format_name_fkey FOREIGN KEY (format_name)
-   REFERENCES discogs.format (name),
-CONSTRAINT releases_formats_release_id_fkey FOREIGN KEY (release_id)
-    REFERENCES discogs.release (id));
+    release_id integer NOT NULL,
+    "position" integer NOT NULL,
+    format_name text,
+    qty numeric(100,0),
+    descriptions text[],
+    CONSTRAINT releases_formats_pkey PRIMARY KEY (release_id, "position"),
+    CONSTRAINT releases_formats_format_name_fkey FOREIGN KEY (format_name)
+        REFERENCES discogs.format (name),
+    CONSTRAINT releases_formats_release_id_fkey FOREIGN KEY (release_id)
+        REFERENCES discogs.release (id));
 
 CREATE TABLE discogs.releases_images (
-
-release_id integer NOT NULL,
-type text NOT NULL,
-height integer,
-width integer,
-image_uri text,
-CONSTRAINT releases_images_pkey PRIMARY KEY (release_id, type));
-
+    release_id integer NOT NULL,
+    type text NOT NULL,
+    height integer,
+    width integer,
+    image_uri text,
+    CONSTRAINT releases_images_pkey PRIMARY KEY (release_id, type));
 
 CREATE TABLE discogs.releases_labels (
-
-label text,
-release_id integer NOT NULL,
-catno text NOT NULL,
-CONSTRAINT releases_labels_pkey PRIMARY KEY (release_id, label, catno));
+    label text,
+    release_id integer NOT NULL,
+    catno text NOT NULL,
+    CONSTRAINT releases_labels_pkey PRIMARY KEY (release_id, label, catno));
 
 CREATE TABLE discogs.role (
-
-role_name text NOT NULL,
-CONSTRAINT role_pkey PRIMARY KEY (role_name));
+    role_name text NOT NULL,
+    CONSTRAINT role_pkey PRIMARY KEY (role_name));
 
 CREATE TABLE discogs.track (
-
-release_id integer,
-"position" text,
-track_id textNOT NULL,
-title text,
-duration text,
-trackno integer,
-CONSTRAINT track_pkey PRIMARY KEY (track_id));
+    release_id integer,
+    "position" text,
+    track_id textNOT NULL,
+    title text,
+    duration text,
+    trackno integer,
+    CONSTRAINT track_pkey PRIMARY KEY (track_id));
 
 CREATE TABLE discogs.track_artists (
-
-track_id text NOT NULL,
-"position" integer NOT NULL,
-artist_id integer,
-artist_name text,
-anv text,
-join_relation text,
-CONSTRAINT tracks_artists_pkey PRIMARY KEY (track_id, "position"));
+    track_id text NOT NULL,
+    "position" integer NOT NULL,
+    artist_id integer,
+    artist_name text,
+    anv text,
+    join_relation text,
+    CONSTRAINT tracks_artists_pkey PRIMARY KEY (track_id, "position"));
 
 CREATE TABLE discogs.tracks_extraartists (
-
-track_id text,
-artist_id integer,
-artist_name text,
-anv text,
-role text,
-data_quality text);
+    track_id text,
+    artist_id integer,
+    artist_name text,
+    anv text,
+    role text,
+    data_quality text);
