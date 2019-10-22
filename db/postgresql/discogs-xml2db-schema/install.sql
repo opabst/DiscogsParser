6441b91a2e7cb8c690252c8f106f66ca6c963a63
Oliver Pabst <oliver.pabst@mailbox.org>
2019-10-22 14:34:02 +0200

    create tables unlogged for performance reasons

--- db/postgresql/discogs-xml2db-schema/install.sql
+++ db/postgresql/discogs-xml2db-schema/install.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA discogs;
 
-CREATE TABLE discogs.artist (
+CREATE UNLOGGED TABLE discogs.artist (
     id integer NOT NULL,
     name text NOT NULL,
     realname text,
@@ -14,28 +14,28 @@ CREATE TABLE discogs.artist (
     data_quality text,
     CONSTRAINT artist_pkey PRIMARY KEY (id));
 
-CREATE TABLE discogs.artists_image(
+CREATE UNLOGGED TABLE discogs.artists_image(
     artist_id integer,
     type text,
     height integer,
     width integer,
     image_uri text);
 
-CREATE TABLE discogs.country (
+CREATE UNLOGGED TABLE discogs.country (
     name text);
 
-CREATE TABLE discogs.format(
+CREATE UNLOGGED TABLE discogs.format(
     name text NOT NULL,
     CONSTRAINT format_pkey PRIMARY KEY (name));
 
-CREATE TABLE discogs.genre (
+CREATE UNLOGGED TABLE discogs.genre (
     id integer NOT NULL,
     name text,
     parent_genre integer,
     sub_genre integer,
     CONSTRAINT genre_pkey PRIMARY KEY (id));
 
-CREATE TABLE discogs.label (
+CREATE UNLOGGED TABLE discogs.label (
     id integer NOT NULL,
     name text NOT NULL,
     contactinfo text,
@@ -46,14 +46,14 @@ CREATE TABLE discogs.label (
     data_quality text,
     CONSTRAINT label_pkey PRIMARY KEY (id));
 
-CREATE TABLE discogs.labels_images (
+CREATE UNLOGGED TABLE discogs.labels_images (
     label_id integer,
     type text,
     height integer,
     width integer,
     image_uri text);
 
-CREATE TABLE discogs.master (
+CREATE UNLOGGED TABLE discogs.master (
     id integer NOT NULL,
     title text,
     main_release integer NOT NULL,
@@ -65,28 +65,28 @@ CREATE TABLE discogs.master (
     data_quality text,
     CONSTRAINT master_pkey PRIMARY KEY (id));
 
-CREATE TABLE discogs.master_artists (
+CREATE UNLOGGED TABLE discogs.master_artists (
     artist_name text,
     master_id integer);
 
-CREATE TABLE discogs.masters_artists_joins (
+CREATE UNLOGGED TABLE discogs.masters_artists_joins (
     artist1 text,
     artist2 text,
     join_relation text,
     master_id integer);
 
-CREATE TABLE discogs.masters_extraartists (
+CREATE UNLOGGED TABLE discogs.masters_extraartists (
     master_id integer,
     artist_name text,
     roles text[]);
 
-CREATE TABLE discogs.masters_formats (
+CREATE UNLOGGED TABLE discogs.masters_formats (
     master_id integer,
     format_name text,
     qty integer,
     descriptions text[]);
 
-CREATE TABLE discogs.masters_images (
+CREATE UNLOGGED TABLE discogs.masters_images (
     master_id integer NOT NULL,
     type text COLLATE,
     height integer,
@@ -94,7 +94,7 @@ CREATE TABLE discogs.masters_images (
     image_uri text,
     CONSTRAINT masters_images_pkey PRIMARY KEY (master_id, type));
 
-CREATE TABLE discogs.release (
+CREATE UNLOGGED TABLE discogs.release (
     id integer NOT NULL,
     status text,
     title text,
@@ -108,7 +108,7 @@ CREATE TABLE discogs.release (
     data_quality text,
     CONSTRAINT release_pkey PRIMARY KEY (id));
 
-CREATE TABLE discogs.releases_artists (
+CREATE UNLOGGED TABLE discogs.releases_artists (
     release_id integer NOT NULL,
     "position" integer NOT NULL,
     artist_id integer,
@@ -117,14 +117,14 @@ CREATE TABLE discogs.releases_artists (
     join_relation text,
     CONSTRAINT releases_artists_pkey PRIMARY KEY (release_id, "position"));
 
-CREATE TABLE discogs.releases_extraartists (
+CREATE UNLOGGED TABLE discogs.releases_extraartists (
     release_id integer,
     artist_id integer,
     artist_name text,
     anv text,
     role text);
 
-CREATE TABLE discogs.releases_formats (
+CREATE UNLOGGED TABLE discogs.releases_formats (
     release_id integer NOT NULL,
     "position" integer NOT NULL,
     format_name text,
@@ -136,7 +136,7 @@ CREATE TABLE discogs.releases_formats (
     CONSTRAINT releases_formats_release_id_fkey FOREIGN KEY (release_id)
         REFERENCES discogs.release (id));
 
-CREATE TABLE discogs.releases_images (
+CREATE UNLOGGED TABLE discogs.releases_images (
     release_id integer NOT NULL,
     type text NOT NULL,
     height integer,
@@ -144,17 +144,17 @@ CREATE TABLE discogs.releases_images (
     image_uri text,
     CONSTRAINT releases_images_pkey PRIMARY KEY (release_id, type));
 
-CREATE TABLE discogs.releases_labels (
+CREATE UNLOGGED TABLE discogs.releases_labels (
     label text NOT NULL,
     release_id integer NOT NULL,
     catno text NOT NULL,
     CONSTRAINT releases_labels_pkey PRIMARY KEY (release_id, label, catno));
 
-CREATE TABLE discogs.role (
+CREATE UNLOGGED TABLE discogs.role (
     role_name text NOT NULL,
     CONSTRAINT role_pkey PRIMARY KEY (role_name));
 
-CREATE TABLE discogs.track (
+CREATE UNLOGGED TABLE discogs.track (
     release_id integer,
     "position" text,
     track_id text NOT NULL,
@@ -163,7 +163,7 @@ CREATE TABLE discogs.track (
     trackno integer,
     CONSTRAINT track_pkey PRIMARY KEY (track_id));
 
-CREATE TABLE discogs.track_artists (
+CREATE UNLOGGED TABLE discogs.track_artists (
     track_id text NOT NULL,
     "position" integer NOT NULL,
     artist_id integer,
@@ -172,7 +172,7 @@ CREATE TABLE discogs.track_artists (
     join_relation text,
     CONSTRAINT tracks_artists_pkey PRIMARY KEY (track_id, "position"));
 
-CREATE TABLE discogs.tracks_extraartists (
+CREATE UNLOGGED TABLE discogs.tracks_extraartists (
     track_id text,
     artist_id integer,
     artist_name text,
